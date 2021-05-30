907076202113290fdd43fdeec74b174b64fd9b9e
Divnsh <divyansh.madhwal@gmail.com>
2021-05-30 20:35:32 +0530

    first

---
+++ Readme.md
@@ -0,0 +1,108 @@
+# Classification Task - JobAThon
+
+
+This is the solution description for the Job-A-Thon held by AnalyticsVidya in May 2021. The description is divided into sections pertaining to *exploratory data analysis*, *feature engineering*, *modelling*, *further improvements*.<br/> 
+Exploratory plots are provided in [**eda.ipynb**](eda.ipynb) notebook. Feature engineering and modelling processes are contained in [**modelling.py**](modelling.py) script.<br/>
+
+
+
+## Exploratory Data Analysis
+
+* Train data consists of 10 features and 2,45,725 entries 
+    * 1 unique id column - one unique id for each entry<br/><br/>
+    * Features 'Gender', 'Region_Code', 'Occupation', 'Channel_Code',
+       'Credit_Product', 'Is_Active' are categorical
+        * Region_Code is a high cardinality feature with 35 unique codes<br/><br/>
+    * 3 features - 'Age', 'Vintage', 'Avg_Account_Balance' - are integer type
+        * All are positively skewed<br/><br/>
+    * 1 feature Credit_Product has missing values (29,325)
+        * Values being missing is strongly associated with the dependent variable<br/><br/>
+    * All 3 numeric features have outliers
+        * Age has outliers in class 1
+        * Vintage has outliers in class 0
+        * Avg_Account_Balance has outliers in both categories<br/><br/>
+    * Vintage and Age have a strong positive correlation (0.6-0.8), rest are very weakly correlated<br/><br/>
+    * Ages are on average higher for class 1 than 0. Vintage shows different distributions for the two classes<br/><br/>
+    * Occupation, Channel_Code, Credit_Product, Is_Active have apparent differences in their distributions with respect to the dependent variable (0/1)
+
+
+
+* Dependent variable is imbalanced - 76% of the values are 0
+
+## Feature Engineering
+
+
+### Pre-processing and engineering
+* 1 dummy feature is extracted from missing values of Credit_Product indicating whether the value is missing or not (0/1).
+* 6 categorical variables are converted to integers using label encoding
+* Outliers of Avg_Account_Balance are capped at upper and lower bounds of their respective features using the inter-quartile range (Q3+1.5*IQR and Q1-1.5*IQR).
+* All non-categorical columns are centered around their mean and scaled by their standard deviation.
+* Categorical features with cardinality > 5 are target mean-encoded using out of fold means with smoothing. Number of folds is 7, alpha is 5.
+
+    
+
+## Modelling
+
+### Metrics for evaluation
+* **ROC_AUC**: A balanced metric for evaluating performance on both positive and negative classes.
+* **F1**: 2\**Precision*\**Recall*/(*Precission*+*Recall*). Harmonic mean of precision and recall of positive class.
+* **F1-micro**: Overall F1 score  of all the classes put together. Reveals the aggregate performance of the model on all classes.
+* **F1-macro**: Average of F1-scores all the classes. Can be low when one of the f1-scores is low.
+* **Accuracy**: Proportion of samples correctly classified. Can be misleading in imbalanced dataset such as ours. 
+* **Precision-Recall AUC**: Does not take True Negatives into account. It is more focused on the positive class and should be preferred
+                            to ROC when positive class is of greater interest.<br/><br/>
+
+### Machine learning algorithms used
+
+An ensemble of linear, tree-based, and neural network models are employed to find relevant patterns in the data for 
+effective supervised learning.
+5-fold cross validation is performed for hyperparameter tuning in each model.
+The ROC and PR curves are plotted for each model and saved in the **plots** directory.<br/>
+
+
+* *Logistic regression*
+    * Logistic regression with *l2* penalty and *balanced* class weights is performed on the whole train dataset, providing an average *ROC AUC* 0.8541315155438103<br/>
+      Best hyperparater: *{'C': 10.0}*<br/>
+      Results on the whole training set: *{'ROC_AUC': 0.854185626143465, 'F1_micro': 0.8015749313256688, 'F1_macro': 0.7540834203020388, 'F1': 0.6460142297081457, 'accuracy': 0.8015749313256689, 'PR_AUC': 0.6940625268699401}*<br/>  
+      This gives a baseline indication of performance of linear models on the given data. <br/><br/>
+* *XGBoost*
+    * Extreme Gradient boosted trees using many weak learners to form a strong learner.
+    * XGBoost uses first and second order gradients of the loss function to arrive at the minima quicker, and each node split is evaluated
+      quickly using these gradients as they are constant for all possibles splits in a particular iteration. Regularization introduced to 
+      the loss makes it less prone to overfitting.
+    * 5-fold cross validation is performed on train for hyperparameters tuning and model evaluation. 
+        * Best hyperparameters are: *{'colsample_bytree': 0.8, 'gamma': 5, 'max_depth': 4, 'min_child_weight': 5, 'subsample': 0.8}*
+        * The best model yields an average ROC AUC of *0.873299*<br/><br/> 
+* *Multi-layer Perceptron*
+    * A neural network is a highly flexible model which can be fine tuned for a variety of tasks.
+    * Hyperparameters are tuned on 5-fold cross validation 
+        * Best hyperparameters are: *{'activation': 'relu', 'alpha': 0.05, 'hidden_layer_sizes': (50, 30, 10), 'learning_rate': 'constant', 'solver': 'adam'}*
+        * Average ROC AUC of the best model is *0.8724521*.<br/><br/> 
+
+* *Stacking*
+    * A stacking classifier (XGBoost classifier) is trained on the outputs of these 3 models.
+        * Evaluation is performed with 5-fold cross-validation.
+        * Average ROC AUC is .
+        * Performance metrics on the whole dataset: 
+    * Finally, predictions are made for the pre-processed test data and saved in the file [**submission.csv**](submission.csv).<br/><br/>
+
+
+## Further possible improvements
+* Grid search for hyperparameter tuning is time consuming and memory hogging. A better approach would be to use bayesian optimization to search in 
+   areas that are likely to give better results. [*Hyperopt*](http://hyperopt.github.io/hyperopt/) is a good alternative. This can allow us to further
+   tune our models.
+* Hyperparameter optimization on the stacking classifier may yield better results. Muti-level stacking could also be beneficial.
+* Introduction of interaction terms from existing variables may provide useful information to the model for learning deeper patterns.<br/><br/>
+
+
+## Major libraries used
+* *pandas*, *numpy* : Reading and processing data
+* *sklearn*, *xgboost*: Data pre-processing and machine learning
+* *matplotlib*, *seaborn*: Data visualization
+* *statsmodels*: Statistical tests and analysis
+
+
+
+
+
+
---
+++ modelling.py
@@ -0,0 +1,311 @@
+import pandas as pd # For data reading and manipulation
+import numpy as np 
+import matplotlib.pyplot as plt # For visualizations
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, plot_roc_curve, auc, f1_score
+from sklearn.metrics import precision_recall_curve, plot_precision_recall_curve, classification_report,accuracy_score
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_score
+import warnings
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import StackingClassifier
+import joblib # saving intermediate results
+
+warnings.filterwarnings('ignore')
+
+
+## Feature selection and engineering
+
+# Reading data
+train=pd.read_csv('data/train_s3TEQDk.csv')
+X_test=pd.read_csv('data/test_mSzZ8RL.csv').drop('ID',1)
+
+X_train = train.drop(['Is_Lead','ID'],axis=1)
+Y_train = train[['Is_Lead']]
+
+
+# Creating binary features from feature with missing values indicating missing values
+class MissingIndicator(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=['Credit_Product']):
+        self.cols = columns
+
+    def fit(self,X,y=None):
+        return self
+
+    def transform(self,X):
+        for col in self.cols:
+            X[col+'_missing']=X[col].isna().astype(int)
+        return X
+
+
+# Converting categorical features to integers
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, encoding='label', handle_unknown='error'):
+        self.encoding = encoding
+        self.handle_unknown = handle_unknown
+
+    def fit(self, X, y=None):
+        """Fit the CategoricalEncoder to X.
+        """
+
+        if self.encoding not in ['onehot', 'label']:
+            template = ("encoding should be either 'onehot' or label, got %s")
+            raise ValueError(template % self.handle_unknown)
+
+        self.features=X.select_dtypes(object).columns.tolist()
+
+        self._label_encoders_ = [LabelEncoder() for _ in range(len(self.features))]
+
+        for i,f in enumerate(self.features):
+            le = self._label_encoders_[i]
+            Xi = X[f]
+            le.fit(Xi)
+
+        self.categories_ = [le.classes_ for le in self._label_encoders_]
+        return self
+
+    def transform(self, X):
+        """Transform X using fit encoding."""
+        for i,f in enumerate(self.features):
+            X[f] = self._label_encoders_[i].transform(X[f])
+        return X
+
+
+# Capping outliers to Q3+1.5*IQR and Q1-1.5*IQR
+class OutlierTreatment(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=['Avg_Account_Balance']):
+        self.cols = columns
+
+    def fit(self,X,y=None):
+        self.bounds={}
+        for col in self.cols:
+            self.bounds[col+'_ul'] = X[col].quantile(0.75) + 1.5*(X[col].quantile(0.75)-X[col].quantile(0.25)) # Upper limit
+            self.bounds[col+'_ll'] = X[col].quantile(0.25) - 1.5*(X[col].quantile(0.75)-X[col].quantile(0.25)) # Lower limit
+        return self
+
+    def transform(self,X):
+        for col in self.cols:
+            X[col]=np.where(X[col]>self.bounds[col+'_ul'],self.bounds[col+'_ul'],X[col])
+            X[col]=np.where(X[col]<self.bounds[col+'_ll'],self.bounds[col+'_ll'],X[col])
+        return X
+
+
+# Centering numeric variables around their mean scaling by standard deviation
+class StandardScaling(BaseEstimator, TransformerMixin):
+    def __init__(self,columns=['Age','Vintage','Avg_Account_Balance']):
+        self.cols=columns
+
+    def fit(self,X,y=None):
+        self.means=X[self.cols].mean()
+        self.stds=X[self.cols].std()
+        return self
+
+    def transform(self,X):
+        X[self.cols]=(X[self.cols]-self.means)/self.stds
+        return X
+
+
+pipeline = Pipeline([
+    ('missing',MissingIndicator()),
+    ('outliers',OutlierTreatment()),
+    ('encoding',CategoricalEncoder()),
+    ('standardizing',StandardScaling())
+])
+X_train=pipeline.fit_transform(X_train)
+X_test=pipeline.transform(X_test)
+
+
+# Function for computing evaluation metrics
+def get_metrics(model,data,true,kind='_model'):
+    metrics={}
+    preds=model.predict(data)
+    metrics['ROC_AUC']=roc_auc_score(true,model.predict_proba(data)[:,1])
+    roc=plot_roc_curve(model,data,true)
+    roc.ax_.set_title('ROC: AUC={0:0.2f}'.format(metrics['ROC_AUC']))
+    plt.savefig(f'plots/ROC_curve{kind}.png')
+    plt.show()
+    print(classification_report(true,preds))
+    metrics['F1_micro']=f1_score(true,preds,average='micro')
+    metrics['F1_macro']=f1_score(true,preds,average='macro')
+    metrics['F1']=f1_score(true,preds)
+    metrics['accuracy']=accuracy_score(true,preds)
+    precision, recall, thresholds=precision_recall_curve(true,model.predict_proba(data)[:,1])  
+    metrics['PR_AUC'] = auc(recall,precision)
+    pr=plot_precision_recall_curve(model,data,true)
+    pr.ax_.set_title('Precision-Recall curve: AUC={0:0.2f}'.format(metrics['PR_AUC']))
+    plt.savefig(f'plots/PR_curve{kind}.png')
+    plt.show()
+    return metrics
+
+
+# Target encoding
+def mean_encode(train_data, test_data, columns, target_col, reg_method=None,
+                alpha=0, add_random=False, rmean=0, rstd=0.1, folds=1):
+    '''Returns a DataFrame with encoded columns'''
+    encoded_cols = []
+    target_mean_global = train_data[target_col].mean()
+    for col in columns:
+        # Getting means for test data
+        nrows_cat = train_data.groupby(col)[target_col].count()
+        target_means_cats = train_data.groupby(col)[target_col].mean()
+        target_means_cats_adj = (target_means_cats*nrows_cat + 
+                                 target_mean_global*alpha)/(nrows_cat+alpha)
+        # Mapping means to test data
+        encoded_col_test = test_data[col].map(target_means_cats_adj)
+        # Getting a train encodings
+        if reg_method == 'expanding_mean':
+            train_data_shuffled = train_data.sample(frac=1, random_state=1)
+            cumsum = train_data_shuffled.groupby(col)[target_col].cumsum() - train_data_shuffled[target_col]
+            cumcnt = train_data_shuffled.groupby(col).cumcount()
+            encoded_col_train = cumsum/(cumcnt)
+            encoded_col_train.fillna(target_mean_global, inplace=True)
+            if add_random:
+                encoded_col_train = encoded_col_train + np.random.normal(loc=rmean, scale=rstd, 
+                                                               size=(encoded_col_train.shape[0]))
+        elif (reg_method == 'k_fold') and (folds > 1):
+            skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=1)
+            parts = []
+            for tr_in, val_ind in skf.split(train_data,train_data[target_col].values):
+                # divide data
+                df_for_estimation, df_estimated = train_data.iloc[tr_in], train_data.iloc[val_ind]
+                # getting means on data for estimation (all folds except estimated)
+                nrows_cat = df_for_estimation.groupby(col)[target_col].count()
+                target_means_cats = df_for_estimation.groupby(col)[target_col].mean()
+                target_means_cats_adj = (target_means_cats*nrows_cat + 
+                                         target_mean_global*alpha)/(nrows_cat+alpha)
+                # Mapping means to estimated fold
+                encoded_col_train_part = df_estimated[col].map(target_means_cats_adj)
+                if add_random:
+                    encoded_col_train_part = encoded_col_train_part + np.random.normal(loc=rmean, scale=rstd, 
+                                                                             size=(encoded_col_train_part.shape[0]))
+                # Saving estimated encodings for a fold
+                parts.append(encoded_col_train_part)
+            encoded_col_train = pd.concat(parts, axis=0)
+            encoded_col_train.fillna(target_mean_global, inplace=True)
+        else:
+            encoded_col_train = train_data[col].map(target_means_cats_adj)
+            if add_random:
+                encoded_col_train = encoded_col_train + np.random.normal(loc=rmean, scale=rstd, 
+                                                               size=(encoded_col_train.shape[0]))
+
+        # Saving the column with means
+        encoded_col = pd.concat([encoded_col_train, encoded_col_test], axis=0)
+        encoded_col[encoded_col.isnull()] = target_mean_global
+        encoded_cols.append(pd.DataFrame({'mean_'+target_col+'_'+col:encoded_col}))
+    all_encoded = pd.concat(encoded_cols, axis=1)
+    return (all_encoded.iloc[:len(train_data),:], 
+            all_encoded.iloc[len(train_data):,:])
+
+
+# Out of fold mean encoding
+X_train_encoded,X_test_encoded=mean_encode(train_data=pd.concat([X_train,Y_train.iloc[:,0]],axis=1), test_data=X_test, columns=['Region_Code','Occupation','Channel_Code','Credit_Product'],
+                target_col='Is_Lead', reg_method='k_fold', alpha=5, add_random=False, rmean=0, rstd=0.1, folds=7)
+
+X_train=pd.concat([X_train,X_train_encoded],axis=1).drop(['Region_Code','Occupation','Channel_Code','Credit_Product'],1)
+X_test=pd.concat([X_test,X_test_encoded],axis=1).drop(['Region_Code','Occupation','Channel_Code','Credit_Product'],1)
+
+# Stratified 5-fold CV
+skf=StratifiedKFold(n_splits=5,shuffle=True, random_state=2021)
+
+# Logistic regression
+from sklearn.linear_model import LogisticRegression
+
+params={'C':np.logspace(-1, 1, 4, endpoint = True, base = 10)}
+logistic=GridSearchCV(LogisticRegression( class_weight='balanced', solver='liblinear', penalty='l2'),params, n_jobs=-1,
+                cv=skf, scoring='roc_auc')
+logistic.fit(X_train,Y_train.iloc[:,0].values)
+print(logistic.best_score_,logistic.best_params_) # Best average score: 0.8541315155438103 {'C': 10.0}
+metrics=get_metrics(logistic,X_train,Y_train.iloc[:,0].values,kind='_logistic')
+print(metrics) # Results on whole train set
+#{'ROC_AUC': 0.854185626143465, 'F1_micro': 0.8015749313256688, 'F1_macro': 0.7540834203020388, 'F1': 0.6460142297081457, 'accuracy': 0.8015749313256689, 'PR_AUC': 0.6940625268699401}
+
+## SVM classification with 5-fold CV - taking too long
+from sklearn.svm import SVC
+from mlxtend.plotting import plot_decision_regions
+# Grid search on SVC
+params={'C':np.logspace(-1, 1, 3, endpoint = True, base = 10),'kernel':['rbf']}
+svm=GridSearchCV(SVC(C=0.5, class_weight='balanced', probability=True, kernel='linear'),params, n_jobs=-1,
+                cv=skf, scoring='roc_auc')
+svm.fit(X_train,Y_train.iloc[:,0].values)
+print(svm.best_score_,svm.best_params_) # Best average score:
+metrics=get_metrics(svm,X_train,Y_train.iloc[:,0].values,kind='_svm')
+print(metrics)
+
+
+
+## XGBoost with 5-fold CV
+params = {
+        'min_child_weight': [5], # min weight in a child: higher=>conservative
+        'gamma': [5], # min loss reduction: higher=>conservative
+        'subsample': [0.8],
+        'colsample_bytree': [0.8],
+        'max_depth': [4]
+        }
+xgclassifier=XGBClassifier(n_estimators=100,eval_metric='auc')
+xgb = GridSearchCV(xgclassifier, params, n_jobs=1, cv=skf,
+                    scoring='roc_auc')
+xgb.fit(X_train,Y_train.iloc[:,0].values)
+print(xgb.best_score_,xgb.best_params_) # Best average score:  0.8732991519144802\
+#{'colsample_bytree': 0.8, 'gamma': 5, 'max_depth': 4, 'min_child_weight': 5, 'subsample': 0.8}
+
+metrics=get_metrics(xgb,X_train,Y_train.iloc[:,0].values,kind='_xgb') # Metrics on whole train data
+print(metrics) #{'ROC_AUC': 0.8787864749731314, 'F1_micro': 0.8626391291077424, 'F1_macro': 0.784462439175051,\
+# 'F1': 0.6546548390067222, 'accuracy': 0.8626391291077424, 'PR_AUC': 0.7590310374769162}
+
+
+## Multi-layer perceptron with 5-fold CV
+params = {
+    'hidden_layer_sizes': [(50,30,10)],
+    'activation': ['relu'],
+    'solver': ['adam'],
+    'alpha': [0.05], # Regularization
+    'learning_rate': ['constant'],
+}
+mlpclf=MLPClassifier(max_iter=300,batch_size='auto',shuffle=True, random_state=2021, verbose=True,
+                     early_stopping=False)
+mlp = GridSearchCV(mlpclf, params, n_jobs=-1, cv=skf, scoring='roc_auc')
+mlp.fit(X_train, Y_train.iloc[:,0].values) 
+print(mlp.best_score_,mlp.best_params_) # Best average score: 0.8724520798070209
+#{'activation': 'relu', 'alpha': 0.05, 'hidden_layer_sizes': (50, 30, 10), 'learning_rate': 'constant', 'solver': 'adam'}
+metrics=get_metrics(mlp,X_train,Y_train.iloc[:,0].values,kind='_mlp') # Metrics on whole train data
+print(metrics) # {'ROC_AUC': 0.8738893457789153, 'F1_micro': 0.8608810662325771, 'F1_macro': 0.7795893875660349,\
+# 'F1': 0.6457329395305456, 'accuracy': 0.8608810662325771, 'PR_AUC': 0.750926704993776}
+
+
+
+## Stacking all models and performing K-fold CV
+estimators = [('logistic', LogisticRegression(C=10,class_weight='balanced', solver='liblinear', penalty='l2')),
+              ('xgboost',xgclassifier),
+              ('mlp',mlpclf)]
+
+sclf = StackingClassifier(estimators= estimators , final_estimator=XGBClassifier(n_estimators=100,eval_metric='auc'))
+
+
+params = {'mlp__activation': ['relu'],
+        'mlp__alpha':[0.05],
+        'mlp__hidden_layer_sizes':[(50, 30, 10)],
+        'mlp__learning_rate':['constant'],
+        'mlp__solver':['adam'],
+        'xgboost__min_child_weight': [5], 
+        'xgboost__gamma': [5], 
+        'xgboost__subsample': [0.8],
+        'xgboost__colsample_bytree': [0.8],
+        'xgboost__max_depth': [4]}
+ 
+stacked = GridSearchCV(estimator=sclf, param_grid=params, cv=StratifiedKFold(n_splits=5,shuffle=True,
+                    random_state=2021), scoring='roc_auc')
+stacked.fit(X_train, Y_train.iloc[:,0].values)
+print(stacked.best_score_) # Best average score: 
+metrics=get_metrics(stacked,train,Y_train.iloc[:,1].values,kind='_stacked') # Metrics on whole train data
+print(metrics)
+
+
+
+## Making test predictions
+submission=pd.read_csv('data/test_mSzZ8RL.csv')[['ID']]
+submission['Is_Lead']=stacked.predict_proba(X_test)[:,1]
+
+submission.to_csv('submission.csv',header=True,index=False) # Writing final submissions
